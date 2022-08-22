54813cc2a5fc93a6bb64644d691d8ec48da28aba
Gabriel <gopxfs@hotmail.com>
2022-08-22 02:37:10 -0300

    Create teacher class

---
+++ teacher.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require './person'
+# Teacher's information/actions in the library.
+class Teacher < Person
+  attr_reader :specialization
+
+  def initialize(age, name, specialization)
+    super
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
