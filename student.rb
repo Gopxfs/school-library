5649ac7064a7efa011971c37c815d668bc2ae1da
Gabriel <gopxfs@hotmail.com>
2022-08-22 02:37:02 -0300

    Create student class

---
+++ student.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require './person'
+# Students' information/actions in the library.
+class Student < Person
+  attr_reader :classroom
+
+  def initialize(age, name, parent_permission, classroom)
+    super
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+end
