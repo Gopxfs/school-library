656a48bf3e4ff207365eab00123790607ac6c923
Gabriel <gopxfs@hotmail.com>
2022-08-23 13:07:06 -0300

    Change code structure to fit rubocop conditions

--- app.rb
+++ app.rb
@@ -1,8 +1,6 @@
 class App
   def run(option)
-    option = option.to_i
-
-    case option
+    case option.to_i
     when 1
       list_books
     when 2
@@ -15,40 +13,33 @@ class App
       add_rental
     when 6
       list_rental
-    when 7
-      puts "Exiting..."
-
-    else
-      puts "Sorry, this option doesn't exist.\n"
-      sleep(0.8)
-      main
     end
   end
-  
+
   def list_books
-    puts "list_books"
+    puts 'list_books'
   end
 
   def list_people
-    puts "list_people"
+    puts 'list_people'
   end
 
   def add_person
-    puts "Do you want to add a student[1] or a teacher[2]? (Input the number)"
+    puts 'Do you want to add a student[1] or a teacher[2]? (Input the number)'
     option = gets.chomp
-    puts "#{option}"
+    puts option.to_s
   end
 
   def add_book
-    puts "create_book"
+    puts 'create_book'
   end
 
   def add_rental
-    puts "create_rental"
+    puts 'create_rental'
   end
 
   def list_rental
-    puts "list_rental"
+    puts 'list_rental'
   end
 end
 
@@ -60,14 +51,19 @@ def main
 [4] Add a book
 [5] Add a rental
 [6] List all rentals for a given person id
-[7] Exit
-
+[7] Exit\n
 Select an option by typing its number
 "
-
-  option = gets.chomp
-  app = App.new
-  app.run(option)
+  option = gets.chomp.to_i
+  if (1..7).include?(option)
+    app = App.new
+    app.run(option)
+  else
+    puts "Sorry, this option doesn't exist.\n"
+    sleep(0.8)
+    main
+  end
+  puts 'Exiting...'
 end
 
 main
