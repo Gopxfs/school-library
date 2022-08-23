abd42c0568b7230cf2df285523d1ded8c0c2bd55
Gabriel <gopxfs@hotmail.com>
2022-08-23 19:10:51 -0300

    Update add_rental method to create an object rental

--- methods/create_rental.rb
+++ methods/create_rental.rb
@@ -1,7 +1,11 @@
-def create_rental(book)
+require './rental'
+
+def create_rental(book, person)
   print 'Date: '
   date = gets.chomp
-  book.add_rental(date)
+  rental = Rental.new(date, book, person)
+  book.add_rental(rental)
+  person.add_rental(rental)
   puts "Rental created successfully!"
   sleep(0.8)
 end
