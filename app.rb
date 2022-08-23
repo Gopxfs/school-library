89413ae54875d7f4fb4f1b7fb085079e3eb14343
Gabriel <gopxfs@hotmail.com>
2022-08-23 12:25:07 -0300

    Create methods base structure and an array of methods

--- app.rb
+++ app.rb
@@ -1,11 +1,11 @@
 class App
-  
   def initialize
-    @options = ["Error", "b"]
+    @options = ["Error", list_books, list_people, create_person, create_book, create_rental, list_rental]
   end
 
   def run(option)
     option = option.to_i
+
     if (1..7) === option
       puts @options[option]
     else
@@ -13,12 +13,36 @@ class App
       sleep(0.8)
       main
     end
+  end
+  
+  def list_books
+    "list_books"
+  end
 
+  def list_people
+    "list_people"
+  end
+
+  def create_person
+    "create_person"
+  end
+
+  def create_book
+    "create_book"
+  end
+
+  def create_rental
+    "create_rental"
+  end
+
+  def list_rental
+    "list_rental"
   end
 end
 
 def main
-  print "Please choose an option by entering a number:
+  print "
+Please choose an option by entering a number:
 1 - List all books
 2 - List all people
 3 - Create a person
@@ -27,7 +51,8 @@ def main
 6 - List all rentals for a given person id
 7 - Exit
 "
-option = gets.chomp
+
+  option = gets.chomp
   app = App.new
   app.run(option)
 end
