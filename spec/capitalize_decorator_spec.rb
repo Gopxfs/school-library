3d729f742a9239d1390d520acfb1261d19b2a7cd
Gabriel <gopxfs@hotmail.com>
2022-08-31 15:07:32 -0300

    Update code structure

--- spec/capitalize_decorator_spec.rb
+++ spec/capitalize_decorator_spec.rb
@@ -1,11 +1,9 @@
 require 'spec_helper'
 describe CapitalizeDecorator do
-  before :each do
+  describe "#correct_name" do
     @person = Person.new(15, "nAME")
     @capitalize_decorator = CapitalizeDecorator.new(@person)
-  end
-
-  describe "#correct_name" do
+    
     it "capitalizes the person's name" do
       expect(@capitalize_decorator.correct_name).to eql "Name"
     end
--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -20,8 +20,10 @@ describe Classroom do
     it "adds a student to the student's array" do
       student = Student.new(15)
       student2 = Student.new(16)
+
       @classroom.add_student(student)
       @classroom.add_student(student2)
+      
       expect(@classroom.students[0]).to eql student
       expect(@classroom.students[1]).to eql student2
     end
