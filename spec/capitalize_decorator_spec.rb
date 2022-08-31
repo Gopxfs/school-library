99ecf30ccadb9c6e2418fd08b5364d21d17b22a3
Gabriel <gopxfs@hotmail.com>
2022-08-31 13:53:00 -0300

    Add test case to class capitalize_decorator

--- spec/capitalize_decorator_spec.rb
+++ spec/capitalize_decorator_spec.rb
@@ -1,1 +1,13 @@
 require 'spec_helper'
+describe CapitalizeDecorator do
+  before :each do
+    @person = Person.new(15, "nAME")
+    @capitalize_decorator = CapitalizeDecorator.new(@person)
+  end
+
+  describe "#correct_name" do
+    it "capitalizes the person's name" do
+      expect(@capitalize_decorator.correct_name).to eql "Name"
+    end
+  end
+end
