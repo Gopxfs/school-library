44485fcba5f1b89613fce527fd4bbffb908738cb
Gabriel <gopxfs@hotmail.com>
2022-08-31 15:11:26 -0300

    Fix identation error

--- spec/capitalize_decorator_spec.rb
+++ spec/capitalize_decorator_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 describe CapitalizeDecorator do
   describe "#correct_name" do
-    @person = Person.new(15, "nAME")
-    @capitalize_decorator = CapitalizeDecorator.new(@person)
-    
     it "capitalizes the person's name" do
+      @person = Person.new(15, "nAME")
+      @capitalize_decorator = CapitalizeDecorator.new(@person)
+    
       expect(@capitalize_decorator.correct_name).to eql "Name"
     end
   end
